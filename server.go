cb6c1bc7c4ace1571f50e2d9c1c4a7c88ec10621
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-22 19:07:14 +0300

    Server Golang

--- server.go
+++ server.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+const port = ":5500"
+
 func main() {
 
+	router := mux.NewRouter()
+	router.HandleFunc("/", rootPage)
+
+	fmt.Println("Serving @ http://127.0.0.1" + port)
+	log.Fatal(http.ListenAndServe(port, router))
+}
+
+func rootPage(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is the root page"))
 }
